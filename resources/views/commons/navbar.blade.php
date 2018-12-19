e96ba912bf7335b0490802a342e4a0ba38dd5fa7
EC2 Default User <ec2-user@ip-172-31-8-154.us-east-2.compute.internal>
2018-12-19 09:34:18 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Microposts</a>
+                <a class="navbar-brand" href="/">fb-attendance</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">

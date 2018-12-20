9f361d032333c6c076922bde0f9e8f60ac3a77e4
EC2 Default User <ec2-user@ip-172-31-8-154.us-east-2.compute.internal>
2018-12-20 07:28:16 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app')
 
 @section('content')
@@ -7,7 +8,7 @@
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the Microposts</h1>
+                <h1>Welcome to the FB-ATTENDANCE</h1>
                 {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
